417125fbef48c375e380317175b11404fa774ab9
Liza <isupova.97@mail.ru>
2023-11-05 15:21:04 +0300

    fix: delete unnecessary calls navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Main from './pages/main/main';
 import styles from './pages/main/main.module.scss';
 import ErrorBoundary from './components/errorBoundary';
 import Details from './components/details';
+import Main from './pages/main';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={import.meta.env.VITE_PUBLIC_URL}>
       <ErrorBoundary fallback={<p className={styles['error-boundary']}>Something went wrong</p>}>
         <Routes>
           <Route path="/" element={<Main />}>
-            <Route path="/" element={<Details />} />
+            <Route index element={<Details />} />
           </Route>
         </Routes>
       </ErrorBoundary>
